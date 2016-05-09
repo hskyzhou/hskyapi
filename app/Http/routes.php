52cfc2681c98bc3f5a480ba4590230eabf230d76
hsky <wen.zhou@bioon.com>
2016-05-09 17:45:26 +0800

    产品列表接口

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,4 +3,5 @@
     Route::group(['prefix' => 'api', 'namespace' => 'Api'], function(){
         Route::get('firstcata', 'CatagoryController@Firstcata');
         Route::get('secondcata', 'CatagoryController@SecondCata');
+        Route::get('projectlist', 'CatagoryController@projectList');
     });
