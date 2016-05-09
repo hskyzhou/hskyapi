f838e590712ab308095ad36c0984da96d8971950
hsky <wen.zhou@bioon.com>
2016-05-09 15:02:07 +0800

    修改二级分类接口

--- app/Http/Controllers/Api/CatagoryController.php
+++ app/Http/Controllers/Api/CatagoryController.php
@@ -22,13 +22,57 @@ class CatagoryController extends Controller
 
 
     public function SecondCata(){
-        $data = [
-            '二级分类1',
-            '二级分类2',
-            '二级分类3',
-            '二级分类4',
-            '二级分类5',
-        ];
+        $parent = request('parent', '');
+        if(empty($parent)){
+            switch($parent){
+                case "语文":
+                    $data = [
+                        '语文二级分类1',
+                        '语文二级分类2',
+                        '语文二级分类3',
+                        '语文二级分类4',
+                        '语文二级分类5',
+                    ];
+                    break;
+                case "数学":
+                    $data = [
+                        '数学二级分类1',
+                        '数学二级分类2',
+                        '数学二级分类3',
+                        '数学二级分类4',
+                        '数学二级分类5',
+                    ];
+                    break;
+                case "英语":
+                    $data = [
+                        '英语二级分类1',
+                        '英语二级分类2',
+                        '英语二级分类3',
+                        '英语二级分类4',
+                        '英语二级分类5',
+                    ];
+                    break;
+                case "化学":
+                    $data = [
+                        '化学二级分类1',
+                        '化学二级分类2',
+                        '化学二级分类3',
+                        '化学二级分类4',
+                        '化学二级分类5',
+                    ];
+                    break;
+                case "物理":
+                    $data = [
+                        '物理二级分类1',
+                        '物理二级分类2',
+                        '物理二级分类3',
+                        '物理二级分类4',
+                        '物理二级分类5',
+                    ];
+                    break;
+            }
+        }
+        
         return response()->json($data);
     }
 }
